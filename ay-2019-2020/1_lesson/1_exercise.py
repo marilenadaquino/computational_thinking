f958e44ae6fc6544ae927b472dd1108364190c51
marilena daquino <marilena.daquino2@unibo.it>
2019-10-24 16:32:05 +0200

    added first class and readme

---
+++ ay-2019-2020/1_lesson/1_exercise.py
@@ -0,0 +1,30 @@
+import csv  
+from collections import Counter
+
+# prepare lists including results
+listTitles = [] 
+sexPistols = []
+after2000 = [] 
+languages = []
+
+# open a csv file
+with open('titles.csv', 'r') as myfile:
+	# create a dictionary
+    reader = csv.DictReader(myfile)    
+    # iterate over rows of the csv file -- over key/value of the dictionary
+    for row in reader: 
+        listTitles.append(row['Title'])
+        if ("sex pistols" in row['Title'].lower() ) or ("sex pistols" in row['Other titles'].lower()) or ("sex pistols" in row['Topics'].lower()):
+        	sexPistols.append(row['Title'])
+        if int(row["Date of publication"]) >=2000:
+        	after2000.append(row["Title"])
+        languages.append(row["Languages"])
+
+# print all titles sorted alphabetically
+sortedListTitles = sorted(listTitles)
+# print(sortedListTitles)
+# print(sexPistols)
+
+count_languages = Counter(languages)
+for key, value in count_languages.items(): 
+    print(key + " is the language of " + str(value) + " books ")
---
+++ ay-2019-2020/README.md
@@ -0,0 +1,12 @@
+# Welcome!
+
+Here the schedule of CT hands-on classes (topics addressed in classes may slightly differ from the scheduling)
+
+ * 23 October 2019 - Installation
+ * 30 October 2019 - Python introduction, basics, and data types: lists
+ * 6 November 2019 - Data types: dictionaries 
+ * 13 November 2019 - Basics of computational linguistics: NLTK
+ * 20 November 2019 - Other data types: tuples and dictionaries (again)
+ * 27 November 2019 - [TBD]
+ * 4 December 2019 - [TBD]
+ * 11 December 2019 - [TBD]
